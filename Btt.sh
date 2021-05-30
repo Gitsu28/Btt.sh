98a7815c20636735b3eeb08d3e15d10ded6b3fbb
Gitsu28 <84932464+Gitsu28@users.noreply.github.com>
2021-05-30 20:23:57 +0700

    Update and rename README.md to Btt.sh

---
+++ Btt.sh
@@ -0,0 +1,12 @@
+# Btt.sh
+#Sudo su
+#apt-get upgrade
+#apt-get update
+#apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev
+#apt install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7 libhwloc-dev
+#git clone https://github.com/xmrig/xmrig.git
+#cd xmrig
+#mkdir build
+#cd build
+#cmake ..
+#make
